5a6ba54d5abe1dc93d26a7d50aebb123f565ee33
Gavin Li <craftgavin@gmail.com>
2013-04-03 00:01:30 +0800

    bug fixed

--- module/Ext/config/module.config.php
+++ module/Ext/config/module.config.php
@@ -94,103 +94,6 @@ return array(
 				'Ext_Brick_Position_Navi_Dropdown' => array(
 					'label' => '导航',
 					'desc' => ''
-<<<<<<< HEAD
-				),
-				'Ext_Brick_Position_Breadcrumb' => array(
-					'label' => '当前位置',
-					'desc' => ''
-				)
-			)
-		),
-		'adgroup' => array(
-			'lable' => '广告图',
-			'ext' => array(
-				'Ext_Brick_AdGroup_Accordion' => array(
-					'label' => '广告图切换',
-					'desc' => ''
-				),
-				'Ext_Brick_AdGroup_FancyTransition' => array(
-					'label' => '广告图切换',
-					'desc' => ''
-				),
-				'Ext_Brick_AdGroup_Transition' => array(
-					'label' => '广告图切换',
-					'desc' => ''
-				),
-				'Ext_Brick_AdGroup_Rotate' => array(
-					'label' => '广告图切换',
-					'desc' => ''
-				),
-				'Ext_Brick_AdGroup_Popup' => array(
-					'label' => '广告图切换',
-					'desc' => ''
-				),
-				'Ext_Brick_AdGroup_Opacity' => array(
-					'label' => '广告图切换',
-					'desc' => ''
-				),
-			)
-		),
-		'html' => array(
-			'label' => 'HTML',
-			'ext' => array(
-				'Ext_Brick_Html_Html' => array(
-					'label' => 'HTML',
-					'desc' => ''
-				)
-			)
-		),
-		'logo' => array(
-			'label' => 'LOGO',
-			'ext' => array(
-				'Ext_Brick_Logo_Logo' => array(
-					'label' => 'HTML',
-					'desc' => ''
-				)
-			)
-		),
-		'im' => array(
-			'label' => 'IM',
-			'ext' => array(
-				'Ext_Brick_Im_Im' => array(
-					'label' => 'IM',
-					'desc' => ''
-				)
-			)
-		),
-		'search' => array(
-			'label' => '搜索',
-			'ext' => array(
-				'Ext_Brick_Search_Input' => array(
-					'label' => '搜索输入框',
-					'desc' => ''
-				),
-				'Ext_Brick_Search_Result' => array(
-					'label' => '搜索结果框',
-					'desc' => ''
-				)
-			)
-		),
-		'other' => array(
-			'label' => '其他',
-			'ext' => array(
-				'Ext_Brick_Attachment' => array(
-					'label' => '附件下载',
-					'desc' => ''
-				),
-				'Ext_Brick_ActionContent' => array(
-					'label' => '控制器内容输出',
-					'desc' => ''
-				),
-				'Ext_Brick_SpriteSurrogate' => array(
-					'label' => '选项卡',
-					'desc' => ''
-				)
-			)
-		)
-	),
-	'brick_path_stack' => array(__DIR__.'/../view'),
-=======
 				),
 				'Ext_Brick_Position_Breadcrumb' => array(
 					'label' => '当前位置',
@@ -294,5 +197,4 @@ return array(
 		)
 	),
 	'brick_path_stack' => array(__DIR__.'/../view'),
->>>>>>> origin/cache-update
 );
