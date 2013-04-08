ca7b262e20bd4bcd5c23249253549666a3448e24
Gavin Li <craftgavin@gmail.com>
2013-04-08 16:18:30 +0800

    bug fixed

--- module/Ext/src/Ext/Form/Im/Im.php
+++ module/Ext/src/Ext/Form/Im/Im.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 namespace Ext\Form\Im;
 
@@ -26,33 +25,4 @@ class Im extends Fieldset
 			'options' => array('label' => '旺旺号码（多个号码以\':\'分隔）')
 		));
     }
-=======
-<?php
-namespace Ext\Form\Im;
-
-use Zend\Form\Fieldset;
-
-class Im extends Fieldset
-{
-	public function __construct($factory)
-	{
-		parent::__construct('params');
-		 
-		$this->add(array(
-			'name' => 'qq',
-			'attributes' => array('type' => 'text'),
-			'options' => array('label' => 'QQ号码（多个号码以\':\'分隔）')
-		));
-		$this->add(array(
-			'name' => 'msn',
-			'attributes' => array('type' => 'text'),
-			'options' => array('label' => 'MSN号码（多个号码以\':\'分隔）')
-		));
-		$this->add(array(
-			'name' => 'ww',
-			'attributes' => array('type' => 'text'),
-			'options' => array('label' => '旺旺号码（多个号码以\':\'分隔）')
-		));
-    }
->>>>>>> origin/cache-update
 }
