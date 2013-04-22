c9deaa686df77999ea50e1d2b413676d30fc37d3
Gavin Li <craftgavin@gmail.com>
2013-04-22 11:58:43 +0800

    im bug removed

---
+++ module/Admin/src/Admin/Controller/UpgradeController.php
@@ -0,0 +1,13 @@
+<?php
+namespace Admin\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Admin\Form\Site\EditForm;
+
+class SiteController extends AbstractActionController 
+{
+	public function antToBeeAction()
+    {
+    	
+    }
+}
